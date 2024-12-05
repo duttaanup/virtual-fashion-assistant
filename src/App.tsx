bde1b95e463ecac8f62bced40d6c9d7b32fb09c4
Anup Dutta <duttanup@amazon.com>
2024-12-05 14:41:43 +0530

    added garments

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 //@ts-nocheck
-import { AppLayout, Box, Button, Cards, Container, ContentLayout, Grid, Header, Link, SegmentedControl, SpaceBetween, Tabs, TopNavigation, Wizard } from "@cloudscape-design/components";
+import { Alert, AppLayout, Box, Button, Cards, Container, ContentLayout, Grid, Header, Link, SegmentedControl, SpaceBetween, Tabs, TopNavigation, Wizard } from "@cloudscape-design/components";
 import { useEffect, useState } from "react";
+import { garmentList } from "./common";
 
 function App() {
   const [activeStepIndex, setActiveStepIndex] = useState(0);
-  const [selectedId, setSelectedId] = useState("male")
+  const [imagecount, setImagecount] = useState(1);
+  const [showalert, setShowalert] = useState(false);
+  const [selectedImage, setSelectedimage] = useState(null);
+  const [selectedId, setSelectedId] = useState("male");
+
 
   const initializeCamera = () => {
     const video = document.getElementById('camera-feed');
@@ -16,24 +21,65 @@ function App() {
       .catch((err) => {
         console.error('Error accessing camera:', err);
       });
+    setImagecount(1);
+    setSelectedimage(null);
+    setShowalert(false);
+  }
+
+  const clearAllCanvas = () => {
+    for (let i = 1; i <= 5; i++) {
+      const canvas = document.getElementById(`camera-canvas${i}`);
+      const context = canvas.getContext('2d');
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      canvas.classList.remove("canvas-photo-selected");
+      canvas.classList.remove("canvas-photo");
+      canvas.classList.add("canvas-photo-hidden");
+    }
+    setImagecount(1);
+    setSelectedimage(null);
+    setShowalert(false);
   }
 
   const capturePhoto = () => {
+    let counter = imagecount;
     const video = document.getElementById('camera-feed');
-    const canvas = document.getElementById('camera-canvas');
+    const canvas = document.getElementById(`camera-canvas${counter}`);
     const context = canvas.getContext('2d');
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
+    canvas.classList.remove("canvas-photo-hidden");
+    canvas.classList.add("canvas-photo");
     context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    const dataURL = canvas.toDataURL('image/jpeg');
-    console.log(dataURL);
+    counter++;
+    setImagecount(counter);
+    setShowalert(false);
+  }
+
+  const selectCanvasImage = (detail) => {
+    const canvas = document.getElementById(detail.target.id);
+    const dataUrl = canvas.toDataURL();
+    // reset all canvas css
+    for (let i = 1; i <= 5; i++) {
+      const tempcanvas = document.getElementById(`camera-canvas${i}`);
+      // remove css
+      tempcanvas.classList.remove("canvas-photo-selected");
+      tempcanvas.classList.remove("canvas-photo");
+      if (detail.target.id == `camera-canvas${i}`) {
+        tempcanvas.classList.add("canvas-photo-selected");
+      } else {
+        tempcanvas.classList.add("canvas-photo");
+      }
+
+    }
+    // on selected disable hover effect
+    canvas.classList.add("canvas-photo-selected");
+    setSelectedimage(dataUrl);
+    setShowalert(false);
   }
 
   useEffect(() => {
-    if(activeStepIndex == 0)
+    setImagecount(1);
+    if (activeStepIndex == 0)
       initializeCamera()
-  })
-
+  }, [activeStepIndex])
 
   return (
     <>
@@ -63,153 +109,122 @@ function App() {
             }
           ><Container fitHeight>
               <Wizard
-                  i18nStrings={{
-                    stepNumberLabel: stepNumber =>
-                      `Step ${stepNumber}`,
-                    collapsedStepsLabel: (stepNumber, stepsCount) =>
-                      `Step ${stepNumber} of ${stepsCount}`,
-                    skipToButtonLabel: (step, stepNumber) =>
-                      `Skip to ${step.title}`,
-                    navigationAriaLabel: "Steps",
-                    cancelButton: "Cancel",
-                    previousButton: "Previous",
-                    nextButton: "Next",
-                    submitButton: "Generate Images",
-                    optional: "optional"
-                  }}
-                  onNavigate={({ detail }) => {
-                      setActiveStepIndex(detail.requestedStepIndex);
-                    }
+                i18nStrings={{
+                  stepNumberLabel: stepNumber =>
+                    `Step ${stepNumber}`,
+                  collapsedStepsLabel: (stepNumber, stepsCount) =>
+                    `Step ${stepNumber} of ${stepsCount}`,
+                  skipToButtonLabel: (step, stepNumber) =>
+                    `Skip to ${step.title}`,
+                  navigationAriaLabel: "Steps",
+                  cancelButton: "Cancel",
+                  previousButton: "Previous",
+                  nextButton: "Next",
+                  submitButton: "Generate Images",
+                  optional: "optional"
+                }}
+
+                onCancel={() => {
+                  setActiveStepIndex(0);
+                  clearAllCanvas();
+                }}
+
+                onNavigate={({ detail }) => {
+                  if (detail.requestedStepIndex == 1 && selectedImage == null) {
+                    setShowalert(true)
+                  } else {
+                    setActiveStepIndex(detail.requestedStepIndex);
                   }
-                  activeStepIndex={activeStepIndex}
-                  steps={[
-                    {
-                      title: "Click a Photo",
-                      description: "",
-                      content: (
-                        <Container fitHeight>
-                          <SpaceBetween size="l" alignItems="center">
-                            <video id="camera-feed" autoPlay></video>
-                            <Button variant="primary" onClick={() => { capturePhoto() }}>Take a Photo</Button>
-                            <canvas id="camera-canvas" style={{ width: "100px"}}></canvas>
+                }
+                }
+                activeStepIndex={activeStepIndex}
+                steps={[
+                  {
+                    title: "Click a Photo",
+                    description: "",
+                    content: (
+                      <Container fitHeight>
+                        <SpaceBetween size="l" alignItems="center">
+                          <video id="camera-feed" playsInline></video>
+                          <SpaceBetween size="l" direction="horizontal">
+                            <Button variant="primary" onClick={() => { capturePhoto() }} disabled={(imagecount > 5)}>Take a Photo</Button>
+                            <Button iconName="refresh" onClick={() => { clearAllCanvas() }} disabled={(imagecount <= 1)}>Clear Photo(s)</Button>
                           </SpaceBetween>
 
-                        </Container>
-                      )
-                    },
-                    {
-                      title: "Choose Garment",
-                      content: (
-                        <Container fitHeight>
-                          <SpaceBetween size="l" alignItems="center">
-                            <SegmentedControl selectedId={selectedId}
-                              onChange={({ detail }) =>
-                                setSelectedId(detail.selectedId)
-                              }
-                              label="Default segmented control"
-                              options={[
-                                { text: "Male", id: "male" },
-                                { text: "Female", id: "female" },
-                              ]}
-                            />
-
-                            <Cards
-                              ariaLabels={{
-                                itemSelectionLabel: (e, t) => `select ${t.name}`,
-                                selectionGroupLabel: "Item selection"
-                              }}
-                              cardDefinition={{
-                                header: item => (
-                                  <Link href="#" fontSize="heading-m">
-                                    {item.name}
-                                  </Link>
-                                ),
-                                sections: [
-                                  {
-                                    id: "description",
-                                    header: "Description",
-                                    content: item => item.description
-                                  },
-                                  {
-                                    id: "type",
-                                    header: "Type",
-                                    content: item => item.type
-                                  },
-                                  {
-                                    id: "size",
-                                    header: "Size",
-                                    content: item => item.size
-                                  }
-                                ]
-                              }}
-                              cardsPerRow={[
-                                { cards: 1 },
-                                { minWidth: 300, cards: 3 }
-                              ]}
-                              items={[
-                                {
-                                  name: "Item 1",
-                                  alt: "First",
-                                  description: "This is the first item",
-                                  type: "1A",
-                                  size: "Small"
-                                },
-                                {
-                                  name: "Item 2",
-                                  alt: "Second",
-                                  description: "This is the second item",
-                                  type: "1B",
-                                  size: "Large"
-                                },
-                                {
-                                  name: "Item 3",
-                                  alt: "Third",
-                                  description: "This is the third item",
-                                  type: "1A",
-                                  size: "Large"
-                                },
-                                {
-                                  name: "Item 4",
-                                  alt: "Fourth",
-                                  description: "This is the fourth item",
-                                  type: "2A",
-                                  size: "Small"
-                                },
-                                {
-                                  name: "Item 5",
-                                  alt: "Fifth",
-                                  description: "This is the fifth item",
-                                  type: "2A",
-                                  size: "Large"
-                                },
-                                {
-                                  name: "Item 6",
-                                  alt: "Sixth",
-                                  description: "This is the sixth item",
-                                  type: "1A",
-                                  size: "Small"
-                                }
-                              ]}
-                              loadingText="Loading resources"
-                              empty={
-                                <Box
-                                  margin={{ vertical: "xs" }}
-                                  textAlign="center"
-                                  color="inherit"
-                                >
-                                  <SpaceBetween size="m">
-                                    <b>No resources</b>
-                                    <Button>Create resource</Button>
-                                  </SpaceBetween>
-                                </Box>
-                              }
-                            />
+                          <SpaceBetween direction="horizontal" size="xl">
+                            <canvas id="camera-canvas1" className="canvas-photo-hidden" onClick={(detail) => { selectCanvasImage(detail) }}></canvas>
+                            <canvas id="camera-canvas2" className="canvas-photo-hidden" onClick={(detail) => { selectCanvasImage(detail) }}></canvas>
+                            <canvas id="camera-canvas3" className="canvas-photo-hidden" onClick={(detail) => { selectCanvasImage(detail) }}></canvas>
+                            <canvas id="camera-canvas4" className="canvas-photo-hidden" onClick={(detail) => { selectCanvasImage(detail) }}></canvas>
+                            <canvas id="camera-canvas5" className="canvas-photo-hidden" onClick={(detail) => { selectCanvasImage(detail) }}></canvas>
                           </SpaceBetween>
-                        </Container>
-                      ),
-                    }
-                  ]}
-                />
+
+                          {
+                            (imagecount > 1) && (
+                              <Box color="text-body-secondary">Select a image to proceed</Box>
+                            )
+                          }
+                          <Alert statusIconAriaLabel="Info" visible={showalert}> Select an image to proceed </Alert>
+                        </SpaceBetween>
+
+                      </Container>
+                    )
+                  },
+                  {
+                    title: "Choose Garment",
+                    content: (
+                      <Container fitHeight>
+                        <SpaceBetween size="l" alignItems="center">
+                          <SegmentedControl selectedId={selectedId}
+                            onChange={({ detail }) =>
+                              setSelectedId(detail.selectedId)
+                            }
+                            label="Default segmented control"
+                            options={[
+                              { text: "Male", id: "male" },
+                              { text: "Female", id: "female" },
+                            ]}
+                          />
+
+                          <Cards
+                            ariaLabels={{
+                              itemSelectionLabel: (e, t) => `select ${t.name}`,
+                              selectionGroupLabel: "Item selection"
+                            }}
+                            cardDefinition={{
+                              sections: [
+                                {
+                                  id: "image_id",
+                                  content: item => (<img src={`./garments/${item.image_id}`} width="100%" />),
+                                  
+                                },
+                              ]
+                            }}
+                            cardsPerRow={[
+                              { cards: 1 },
+                              { minWidth: 300, cards: 3 }
+                            ]}
+                            items={garmentList.filter(e => e.gender == selectedId)}
+                            loadingText="Loading resources"
+                            empty={
+                              <Box
+                                margin={{ vertical: "xs" }}
+                                textAlign="center"
+                                color="inherit"
+                              >
+                                <SpaceBetween size="m">
+                                  <b>No resources</b>
+                                  <Button>Create resource</Button>
+                                </SpaceBetween>
+                              </Box>
+                            }
+                          />
+                        </SpaceBetween>
+                      </Container>
+                    ),
+                  }
+                ]}
+              />
             </Container>
           </ContentLayout>
         }
---
+++ src/common.tsx
@@ -0,0 +1,14 @@
+export const garmentList = [
+    { "gender" : "male" , "image_id" : "m_1.jpg"},
+    { "gender" : "male" , "image_id" : "m_2.jpg"},
+    { "gender" : "male" , "image_id" : "m_3.jpg"},
+    { "gender" : "male" , "image_id" : "m_4.jpg"},
+    { "gender" : "male" , "image_id" : "m_5.jpg"},
+    { "gender" : "male" , "image_id" : "m_6.jpg"},
+    { "gender" : "female" , "image_id" : "f_1.jpg"},
+    { "gender" : "female" , "image_id" : "f_2.jpg"},
+    { "gender" : "female" , "image_id" : "f_3.jpg"},
+    { "gender" : "female" , "image_id" : "f_4.jpg"},
+    { "gender" : "female" , "image_id" : "f_5.jpg"},
+    { "gender" : "female" , "image_id" : "f_6.jpg"},
+]
